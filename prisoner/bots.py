32e81b548ec5ce72ea86b0622a35bbfe324611f2
Evan Hubinger <evanjhub@gmail.com>
2015-11-22 19:42:25 -0800

    Updates to new Coconut

--- prisoner/bots.py
+++ prisoner/bots.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# __coconut_hash__ = 0x1b248090
+# __coconut_hash__ = 0xaff07a37
 
 # Compiled with Coconut version 0.3.4-post_dev [Macapuno]
 
@@ -9,30 +9,33 @@
 from __future__ import with_statement, print_function, absolute_import, unicode_literals, division
 import sys as _coconut_sys
 if _coconut_sys.version_info < (3,):
-    py2_filter, py2_hex, py2_map, py2_oct, py2_zip = filter, hex, map, oct, zip
+    _coconut_encoding = "UTF-8"
+    py2_filter, py2_hex, py2_map, py2_oct, py2_zip, py2_open, py2_range, py2_int, py2_chr, py2_str, py2_print, py2_input = filter, hex, map, oct, zip, open, range, int, chr, str, print, input
+    _coconut_int, _coconut_long, _coconut_str, _coconut_bytearray, _coconut_print, _coconut_unicode, _coconut_raw_input = int, long, str, bytearray, print, unicode, raw_input
+    range, chr, str = xrange, unichr, unicode
     from future_builtins import *
-    py2_range, range = range, xrange
-    py2_int = int
-    _coconut_int, _coconut_long = int, long
+    from io import open
     class _coconut_metaint(type):
         def __instancecheck__(cls, inst):
             return isinstance(inst, (_coconut_int, _coconut_long))
     class int(_coconut_int):
         """Python 3 int."""
         __metaclass__ = _coconut_metaint
-    py2_chr, chr = chr, unichr
-    bytes, str = str, unicode
-    _coconut_encoding = "UTF-8"
-    py2_print = print
-    _coconut_print = print
+    class _coconut_metabytes(type):
+        def __instancecheck__(cls, inst):
+            return isinstance(inst, _coconut_str)
+    class bytes(_coconut_str):
+        """Python 3 bytes."""
+        __metaclass__ = _coconut_metabytes
+        def __new__(cls, *args, **kwargs):
+            """Python 3 bytes constructor."""
+            return _coconut_str.__new__(cls, _coconut_bytearray(*args, **kwargs))
     def print(*args, **kwargs):
         """Python 3 print."""
-        return _coconut_print(*(str(x).encode(_coconut_encoding) for x in args), **kwargs)
-    py2_input = raw_input
-    _coconut_input = raw_input
+        return _coconut_print(*(_coconut_unicode(x).encode(_coconut_encoding) for x in args), **kwargs)
     def input(*args, **kwargs):
         """Python 3 input."""
-        return _coconut_input(*args, **kwargs).decode(_coconut_encoding)
+        return _coconut_raw_input(*args, **kwargs).decode(_coconut_encoding)
 
 import os.path as _coconut_os_path
 _coconut_sys.path.append(_coconut_os_path.dirname(_coconut_os_path.abspath(__file__)))
